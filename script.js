4cac607c323e12cfb2fc173376232b5beb3ad82b
slauko <slauko94@gmail.com>
2022-02-15 09:59:04 +0100

    first js code

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+let taskList = []
+class taskItem {
+    constructor(message){
+        this.msg = message;
+        this.done = false;
+    }
+    editMessage(message){
+        this.msg = message;
+    }
+    editDone(){
+        this.done = !this.done;
+    }
+}
+
+const updateTaskList_HTML = () => {
+    console.log("---------------")
+    let list = document.querySelector("#tasklist .list")
+    console.log(list.innerHTML)
+    taskList.forEach(task => {
+        console.log(task.msg)
+    });
+}
+
+const addNewTask = () => {
+    //get message from top textfield and create new task with current message
+    let message = document.querySelector("#newtaskTextfield").value;
+    if (message.length>0) {
+        taskList[taskList.length] = new taskItem(message); 
+    }
+    updateTaskList_HTML();
+}
+
 const hallo = () => {
     console.log("HALLO!")
 }
