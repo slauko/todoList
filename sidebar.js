a3626682891950b8200c497cb2924719089165c3
Kevin Poppe <kevin.poppe93@gmail.com>
2022-02-17 16:08:48 +0100

    modified delte for sidebar

--- sidebar.js
+++ sidebar.js
@@ -1,25 +1,37 @@
-const btn = document.querySelector("#burgerBtnDiv");
+const sidebarBurgerBtn = document.querySelector("#burgerBtnDiv");
+const sidebarAddListBtn = document.querySelector("#sidebarAddBtn");
+const sidebarAddListText = document.querySelector("#newListText");
+const sidebarDeleteBtn = document.querySelector("#sidebarDeleteBtn");
 
-btn.addEventListener("click", changeWidth);
+sidebarBurgerBtn.addEventListener("click", changeWidth);
+sidebarAddListBtn.addEventListener("click", addNewList);
+sidebarAddListText.addEventListener("keyup", (e) => e.key === "Enter" ? addNewList : null);
+sidebarDeleteBtn.addEventListener("click", deleteListItem);
+
+let counter = 0;
+
 
 
 function changeWidth(e){
 
     const sidebar = document.querySelector("#sidebarsection");
-    //const sidebarList = document.querySelector("#sidebarList");
+    //const sidebarContent = document.querySelector("#sidebarContent");
 
-    //console.log(sidebar.offsetWidth); //The HTMLElement.offsetWidth read-only property returns the layout width of an element as an integer
+    //console.log(sidebar.offsetWidth);                           //The HTMLElement.offsetWidth read-only property returns the layout width of an element as an integer
 
-    if(sidebar.offsetWidth == 68){
+
+    //Check if sidebar is extended or retracted:
+    if(sidebar.offsetWidth == 68){                              //Sidebar is retracted -> extend it
 
         sidebar.style.width = "375px";
-        //sidebarList.style.display = "unset";
+        //sidebarContent.style.display = "unset";
 
         hideSidebarContent(false);
 
-    } else {
+    } else {                                                    //Sidebar is extended -> retract it
+
         sidebar.style.width = "68px";
-        //sidebarList.style.display = "none";
+        //sidebarContent.style.display = "none";
 
         hideSidebarContent(true);
     }
@@ -29,21 +41,73 @@ function changeWidth(e){
 
 function hideSidebarContent(hide){
 
-    const sidebarList = document.querySelector("#sidebarList");
+    const sidebarContent = document.querySelector("#sidebarContent");
 
     switch (hide){
 
         case false:
-            sidebarList.style.display = "unset";
+            sidebarContent.style.display = "unset";
             break;
 
         case true:
-            sidebarList.style.display = "none";
+            sidebarContent.style.display = "none";
             break;
 
         default:
             break;
 
     }
+
+}
+
+
+function addNewList(e){
+
+    const listName = document.querySelector("#newListText");                    //Text aus dem input lesen
+    const list = document.querySelector("#sidebarList");
+
+    if(listName === ""){                                                        //Überprüfen, ob Textfeld ausgefüllt ist
+
+        window.alert("Bitte gib einen Namen ein!");
+
+    }else{
+
+        let newLi = createNewLi(listName.value);                                //Neues Element erzeugen
+
+        list.appendChild(newLi);                                                //Neues Element zur Liste hinzufügen
+
+        listName.value = "";                                                    //textfeld zurücksetzen
+
+    }
+
+}
+
+function createNewLi(listName){
+
+    const newItem = document.createElement("li");
+    newItem.className = "sidebarListItem";
+    newItem.innerText = listName;
+    newItem.id = `ListItem${counter}`;
+
+    counter++;
+
+    return newItem;
+
+}
+
+
+function deleteListItem(){
+
+    const list = document.querySelector("#sidebarList");
+
+    list.addEventListener("click", (e) => {
+
+        document.querySelector("#" + e.target.id).remove();
+
+        return null;
+
+    })
+
+    return null;
 
 }
